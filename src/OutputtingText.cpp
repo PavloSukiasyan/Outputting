478b850103d5cf3bbca38e7e075142dd0053c9e7
PavloSukiasyan <pavlo.sukiasyan@gmail.com>
2018-02-15 21:32:55 +0200

    first commit C++

---
+++ src/OutputtingText.cpp
@@ -0,0 +1,20 @@
+//============================================================================
+// Name        : OutputtingText.cpp
+// Author      : Pavlo Sukiasyan
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	cout << "Start a Program..." << flush;
+	cout << "This is a text. " << endl;
+	cout << "Banana_" << " " << "Apple " << "Orange " << endl;
+	cout << "This is a New text. " << endl;
+	return 0;
+}
